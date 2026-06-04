a0bcc2b4bc0f6d5081b8a3112a2bc0a02f26e557
emmanoelb03 <emmanoel.barbosa03@gmail.com>
2026-06-04 18:46:55 -0300

    refactor: remove unnecessary line and specify auth type in firebaseConfig

--- src/utils/firebaseConfig.ts
+++ src/utils/firebaseConfig.ts
@@ -1,7 +1,6 @@
 import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
 
-
 const firebaseConfig = {
   apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
   authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
@@ -14,6 +13,6 @@ const firebaseConfig = {
 };
 
 const app = initializeApp(firebaseConfig);
-const auth:any = getAuth(app);
+const auth = getAuth(app);
 
 export { auth };
